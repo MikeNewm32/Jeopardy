54f68ade28df7c800d2fe8518da1f0ffd702e045
MikeNewm32 <michaelnewman85@hotmail.com>
2017-07-13 15:20:19 -0400

    button funcions

--- public/logic.js
+++ public/logic.js
@@ -1,1 +1,79 @@
 
+// function whenClick(){
+//     prompt("Yo");
+// };
+$(".cat1quest1").on('click',function() {
+    prompt(question11);
+});
+$(".cat1quest2").on('click',function(question12) {
+    whenClick(prompt());
+});
+$(".cat1quest3").on('click',function(question13) {
+    whenClick(prompt());
+});
+$(".cat1quest4").on('click',function(question14) {
+    whenClick(prompt());
+});
+$(".cat1quest5").on('click',function(question15) {
+    whenClick(prompt());
+});
+$(".cat2quest1").on('click',function(question21) {
+    whenClick(prompt());
+});
+$(".cat2quest2").on('click',function(question22) {
+    whenClick(prompt());
+});
+$(".cat2quest3").on('click',function(question23) {
+    whenClick(prompt());
+});
+$(".cat2quest4").on('click',function(question24) {
+    whenClick(prompt());
+});
+$(".cat2quest5").on('click',function(question25) {
+    whenClick(prompt());
+});
+$(".cat3quest1").on('click',function(question31) {
+    whenClick(prompt());
+});
+$(".cat3quest2").on('click',function(question32) {
+    whenClick(prompt());
+});
+$(".cat3quest3").on('click',function(question33) {
+    whenClick(prompt());
+});
+$(".cat3quest4").on('click',function(question34) {
+    whenClick(prompt());
+});
+$(".cat3quest5").on('click',function(question35) {
+    whenClick(prompt());
+});
+$(".cat4quest1").on('click',function(question41) {
+    whenClick(prompt());
+});
+$(".cat4quest2").on('click',function(question42) {
+    whenClick(prompt());
+});
+$(".cat4quest3").on('click',function(question43) {
+    whenClick(prompt());
+});
+$(".cat4quest4").on('click',function(question44) {
+    whenClick(prompt());
+});
+$(".cat4quest5").on('click',function(question45) {
+    whenClick(prompt());
+});
+$(".cat5quest1").on('click',function(question51) {
+    whenClick(prompt());
+});
+$(".cat5quest2").on('click',function(question52) {
+    whenClick(prompt());
+});
+$(".cat5quest3").on('click',function(question53) {
+    whenClick(prompt());
+});
+$(".cat5quest4").on('click',function(question54) {
+    whenClick(prompt());
+});
+$(".cat5quest5").on('click',function(question55) {
+    whenClick(prompt());
+});
