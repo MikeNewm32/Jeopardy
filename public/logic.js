5d11a0cd3c4a30e844dfd852df1bd5315e7564ae
MikeNewm32 <michaelnewman85@hotmail.com>
2017-07-14 15:43:27 -0400

    Removed all modal code for now

--- public/logic.js
+++ public/logic.js
@@ -100,28 +100,3 @@ var question53 = "What amphibians do you raise if your run a ranarium? Frogs.";
 var question54 = "What mollusk was obliged to share its name with the first waterproof watch? The oyster.";
 var question55 = "What tree family includes the largest and fastest-growing living thing on Earth? The Sequoia.";
 
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
