9347a8bcf75397aa5274acfacca103ae417cb6f0
MikeNewm32 <michaelnewman85@hotmail.com>
2017-07-14 15:00:17 -0400

    Tried adding modals but putting off for now

--- public/logic.js
+++ public/logic.js
@@ -1,84 +1,80 @@
-
-// function whenClick(){
-//     prompt("Yo");
-// };
-$(".cat1quest1").on('click',function() {
+$("#cat1quest1").on('click',function() {
     prompt(question11);
 });
-$(".cat1quest2").on('click',function() {
+$("#cat1quest2").on('click',function() {
     prompt(question12);
 });
-$(".cat1quest3").on('click',function() {
+$("#cat1quest3").on('click',function() {
     prompt(question13);
 });
-$(".cat1quest4").on('click',function() {
+$("#cat1quest4").on('click',function() {
     prompt(question14);
 });
-$(".cat1quest5").on('click',function() {
+$("#cat1quest5").on('click',function() {
     prompt(question15);
 });
-$(".cat2quest1").on('click',function() {
+$("#cat2quest1").on('click',function() {
     prompt(question21);
 });
-$(".cat2quest2").on('click',function() {
+$("#cat2quest2").on('click',function() {
     prompt(question22);
 });
-$(".cat2quest3").on('click',function() {
+$("#cat2quest3").on('click',function() {
     prompt(question23);
 });
-$(".cat2quest4").on('click',function() {
+$("#cat2quest4").on('click',function() {
     prompt(question24);
 });
-$(".cat2quest5").on('click',function() {
+$("#cat2quest5").on('click',function() {
     prompt(question25);
 });
-$(".cat3quest1").on('click',function() {
+$("#cat3quest1").on('click',function() {
     prompt(question31);
 });
-$(".cat3quest2").on('click',function() {
+$("#cat3quest2").on('click',function() {
     prompt(question32);
 });
-$(".cat3quest3").on('click',function() {
+$("#cat3quest3").on('click',function() {
     prompt(question33);
 });
-$(".cat3quest4").on('click',function() {
+$("#cat3quest4").on('click',function() {
     prompt(question34);
 });
-$(".cat3quest5").on('click',function() {
+$("#cat3quest5").on('click',function() {
     prompt(question35);
 });
-$(".cat4quest1").on('click',function() {
+$("#cat4quest1").on('click',function() {
     prompt(question41);
 });
-$(".cat4quest2").on('click',function() {
+$("#cat4quest2").on('click',function() {
     prompt(question42);
 });
-$(".cat4quest3").on('click',function() {
+$("#cat4quest3").on('click',function() {
     prompt(question43);
 });
-$(".cat4quest4").on('click',function() {
+$("#cat4quest4").on('click',function() {
     prompt(question44);
 });
-$(".cat4quest5").on('click',function() {
+$("#cat4quest5").on('click',function() {
     prompt(question45);
 });
-$(".cat5quest1").on('click',function() {
+$("#cat5quest1").on('click',function() {
     prompt(question51);
 });
-$(".cat5quest2").on('click',function() {
+$("#cat5quest2").on('click',function() {
     prompt(question52);
 });
-$(".cat5quest3").on('click',function() {
+$("#cat5quest3").on('click',function() {
     prompt(question53);
 });
-$(".cat5quest4").on('click',function() {
+$("#cat5quest4").on('click',function() {
     prompt(question54);
 });
-$(".cat5quest5").on('click',function() {
+$("#cat5quest5").on('click',function() {
     prompt(question55);
 });
 
-var question11 = "The Cuban Missile Crisis of 1962 was a confrontation between which two nations? USA and USSR";
+var question11 = "The Cuban Missile Crisis of 1962 was a confrontation between which two nations?";
 var question12 = "During WWII, a research and development project that produced the 1st nuclear weapons became known as what? The Manhattan Project";
 var question13 = "What year did the attack on Pearl Harbor take place? 1941";
 var question14 = "The Bay of Pigs Invasion was a failed military invasion to overthrow the powers of which controversial leader? Fidel Castro";
@@ -103,3 +99,29 @@ var question52 = "What mineral accounts for 35 percent of the Earth's compositio
 var question53 = "What amphibians do you raise if your run a ranarium? Frogs.";
 var question54 = "What mollusk was obliged to share its name with the first waterproof watch? The oyster.";
 var question55 = "What tree family includes the largest and fastest-growing living thing on Earth? The Sequoia.";
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
